2288be215aea4e350201feb97034083b77ec4e6a
unknown <true.for.fun@gmail.com>
2024-06-11 23:23:39 +0300

    getting rid of typing types by using object properties

--- src/Reduce.js
+++ src/Reduce.js
@@ -17,6 +17,13 @@ const reducer = (state, action) => {
   }
 };
 
+const ACTION = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  NEW_USER_INPUT: "newUserInput",
+  TG_COLOR: "tgColor",
+};
+
 const Reduce = () => {
   const [state, dispatch] = useReducer(reducer, {
     count: 0,
@@ -31,21 +38,29 @@ const Reduce = () => {
         type="text"
         value={state.userInput}
         placeholder="Reduce Input"
+        // onChange={(e) =>
+        //   dispatch({ type: "newUserInput", payload: e.target.value })
         onChange={(e) =>
-          dispatch({ type: "newUserInput", payload: e.target.value })
+          dispatch({ type: ACTION.NEW_USER_INPUT, payload: e.target.value })
         }
       />
       <p>{state.count}</p>
       <section>
-        <button onClick={() => dispatch({ type: "increment" })}>+</button>
+        {/* <button onClick={() => dispatch({ type: "increment" })}>+</button> */}
+        <button onClick={() => dispatch({ type: ACTION.INCREMENT })}>+</button>
         <br />
-        <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+        {/* <button onClick={() => dispatch({ type: "decrement" })}>-</button> */}
+        <button onClick={() => dispatch({ type: ACTION.DECREMENT })}>-</button>
         <br />
-        <button onClick={() => dispatch({ type: "tgColor" })}>
+        {/* <button onClick={() => dispatch({ type: "tgColor" })}>
+          Change Color
+        </button> */}
+        <button onClick={() => dispatch({ type: ACTION.TG_COLOR })}>
           Change Color
         </button>
         <br />
       </section>
+      <p>{state.userInput}</p>
     </div>
   );
 };
